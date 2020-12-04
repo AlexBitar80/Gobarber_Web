bc5479a9ba62cc8386f8147c5dc24fbb6f7a0e4f
Alexandre Bitar <joabitar76@gmail.com>
2020-12-03 22:28:21 -0300

    feat: added styles for the sign-in page

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import sigInBackgroundImg from '../../assets/sign-in-background.png';
+import signInBackgroundImg from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -64,6 +64,6 @@ export const Content = styled.div`
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${sigInBackgroundImg}) no-repeat center;
+  background: url(${signInBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
