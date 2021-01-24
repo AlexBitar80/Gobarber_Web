6f05b4ce5d4ac30e8bf24df219a99cbeaeefaff5
Alexandre Bitar <joabitar76@gmail.com>
2021-01-23 23:11:35 -0300

    chore: list added with appointments on the day

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +41,117 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 23</span>
+            <span>Sábado</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/56983783?s=460&u=08a7f4ee7ae1cbffc824c5fe27d7f81c891a2634&v=4"
+                alt="João Alexandre"
+              />
+
+              <strong>João Alexandre</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/56983783?s=460&u=08a7f4ee7ae1cbffc824c5fe27d7f81c891a2634&v=4"
+                  alt="João Alexandre"
+                />
+                <strong>João Alexandre</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/56983783?s=460&u=08a7f4ee7ae1cbffc824c5fe27d7f81c891a2634&v=4"
+                  alt="João Alexandre"
+                />
+                <strong>João Alexandre</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/56983783?s=460&u=08a7f4ee7ae1cbffc824c5fe27d7f81c891a2634&v=4"
+                  alt="João Alexandre"
+                />
+                <strong>João Alexandre</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/56983783?s=460&u=08a7f4ee7ae1cbffc824c5fe27d7f81c891a2634&v=4"
+                  alt="João Alexandre"
+                />
+                <strong>João Alexandre</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars.githubusercontent.com/u/56983783?s=460&u=08a7f4ee7ae1cbffc824c5fe27d7f81c891a2634&v=4"
+                  alt="João Alexandre"
+                />
+                <strong>João Alexandre</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
