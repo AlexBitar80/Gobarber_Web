b4294b60f307b821ac13130dda9613ad629b0572
Alexandre Bitar <joabitar76@gmail.com>
2021-01-27 23:44:04 -0300

    chore: added navigation to user profile page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -9,6 +9,7 @@ import DayPicker, { DayModifiers } from 'react-day-picker';
 import { format, parseISO, isToday, isAfter } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { Link } from 'react-router-dom';
 import {
   Container,
   Header,
@@ -147,7 +148,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
